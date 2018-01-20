1a038c346d8166ed45a961d28f8a84df364e06a0
teomrd <mironidis.gr@gmail.com>
2018-01-20 14:15:58 +0000

    save the current state and retreive it after page refresh

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+/**
+ * Returns a hash code for a string.
+ * (Compatible to Java's String.hashCode())
+ *
+ * The hash code for a string object is computed as
+ *     s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1]
+ * using number arithmetic, where s[i] is the i th character
+ * of the given string, n is the length of the string,
+ * and ^ indicates exponentiation.
+ * (The hash value of the empty string is zero.)
+ *
+ * @param {string} s a string
+ * @return {number} a hash code value for the given string.
+ */
+hashCode = function(s) {
+  var h = 0,
+    l = s.length,
+    i = 0;
+  if (l > 0) while (i < l) h = ((h << 5) - h + s.charCodeAt(i++)) | 0;
+  return h;
+};
+
 function prettifyJSON(selector) {
   const el = document.querySelector(selector);
   prettifiedJSON = JSON.stringify(JSON.parse(el.value), null, 2);
@@ -6,10 +28,9 @@ function prettifyJSON(selector) {
 
 function saveToLocalStorage(what) {
   if (what.length) {
-    localStorage.setItem(
-      new Date().toDateString() + " " + new Date().toTimeString(),
-      what
-    );
+    const hash = hashCode(what);
+    localStorage.setItem(hash, what);
+    window.location.assign(`#${hash}`);
   } else {
     alert("Nothing to save!");
   }
@@ -29,6 +50,15 @@ function ListenToKeyboard(e) {
   }
 }
 
+function getSavedState() {
+  const hash = window.location.hash.substr(1);
+  const savedTxt = localStorage.getItem(hash);
+  console.log("savedTxt", savedTxt);
+  const el = document.querySelector(".terminal");
+  el.value = savedTxt;
+}
+
 (function() {
   document.onkeydown = ListenToKeyboard;
+  getSavedState();
 })();
