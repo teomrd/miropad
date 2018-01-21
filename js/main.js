86398c1b31cdba26893dbeab51b7ff9c956208cf
teomrd <mironidis.gr@gmail.com>
2018-01-21 12:20:46 +0000

    Added parcel as a builder tool and chagned the fn to es6

--- js/main.js
+++ js/main.js
@@ -1,32 +1,13 @@
-/**
- * Returns a hash code for a string.
- * (Compatible to Java's String.hashCode())
- *
- * The hash code for a string object is computed as
- *     s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1]
- * using number arithmetic, where s[i] is the i th character
- * of the given string, n is the length of the string,
- * and ^ indicates exponentiation.
- * (The hash value of the empty string is zero.)
- *
- * @param {string} s a string
- * @return {number} a hash code value for the given string.
- */
-hashCode = function(s) {
-  var h = 0,
-    l = s.length,
-    i = 0;
-  if (l > 0) while (i < l) h = ((h << 5) - h + s.charCodeAt(i++)) | 0;
-  return h;
-};
+import classes from "../css/styles.css";
+import hashCode from "./utils/hashCode";
 
-function prettifyJSON(selector) {
+const prettifyJSON = selector => {
   const el = document.querySelector(selector);
   prettifiedJSON = JSON.stringify(JSON.parse(el.value), null, 2);
   el.value = prettifiedJSON;
-}
+};
 
-function saveToLocalStorage(what) {
+const saveToLocalStorage = what => {
   if (what.length) {
     const hash = hashCode(what);
     localStorage.setItem(hash, what);
@@ -34,9 +15,9 @@ function saveToLocalStorage(what) {
   } else {
     alert("Nothing to save!");
   }
-}
+};
 
-function ListenToKeyboard(e) {
+const ListenToKeyboard = e => {
   var evtobj = window.event ? event : e;
   // Control + p
   if (evtobj.keyCode == 80 && evtobj.ctrlKey) {
@@ -49,17 +30,19 @@ function ListenToKeyboard(e) {
     const text = document.querySelector(".terminal").value;
     saveToLocalStorage(text);
   }
-}
+};
 
-function getSavedState() {
+const getSavedState = () => {
   const hash = window.location.hash.substr(1);
   const savedTxt = localStorage.getItem(hash);
-  console.log("savedTxt", savedTxt);
   const el = document.querySelector(".terminal");
   el.value = savedTxt;
-}
+};
 
-(function() {
+const main = () => {
+  console.log("main");
   document.onkeydown = ListenToKeyboard;
   getSavedState();
-})();
+};
+
+export default main;
---
+++ js/utils/hashCode.js
@@ -0,0 +1,21 @@
+/**
+ * Returns a hash code for a string.
+ * (Compatible to Java's String.hashCode())
+ *
+ * The hash code for a string object is computed as
+ *     s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1]
+ * using number arithmetic, where s[i] is the i th character
+ * of the given string, n is the length of the string,
+ * and ^ indicates exponentiation.
+ * (The hash value of the empty string is zero.)
+ *
+ * @param {string} s a string
+ * @return {number} a hash code value for the given string.
+ */
+export default function hashCode(s) {
+  const h = 0;
+  const l = s.length;
+  const i = 0;
+  if (l > 0) while (i < l) h = ((h << 5) - h + s.charCodeAt(i++)) | 0;
+  return h;
+}
