89e5d2f8b7b479eaddae9e0851e1f862f1ec57a3
teomrd <mironidis.gr@gmail.com>
2018-03-19 22:40:58 +0000

    lint enabled and fixed all the lint errors form notify

--- src/js/notify.js
+++ src/js/notify.js
@@ -1,47 +1,47 @@
 import select from './utils/dom';
-/* eslint-disable */
+
 const notificationTypes = {
   info: 'info',
   success: 'success',
   error: 'error',
   warning: 'warning',
 };
+
 const notification = select('#notification');
-notification.el.onclick = () => cleanNotificationClasses();
 const cleanNotificationClasses = () => {
   const classes = Object.values(notificationTypes);
   notification.removeClasses(classes);
 };
+notification.el.onclick = () => cleanNotificationClasses();
 
 const Notification = ({ autohideDuration = 5 } = {}) => ({
   autohideDuration,
   timer: undefined,
-  _removeAfter() {
+  removeAfter() {
     this.timer = setTimeout(() => {
       cleanNotificationClasses();
     }, this.autohideDuration * 1000);
   },
-  _showNotification(message, type) {
+  showNotification(message, type) {
     cleanNotificationClasses();
     notification.html(message).addClass(type);
     clearTimeout(this.timer);
-    this._removeAfter();
+    this.removeAfter();
   },
   sucess(message) {
-    this._showNotification(message, notificationTypes.success);
+    this.showNotification(message, notificationTypes.success);
   },
   info(message) {
-    this._showNotification(message, notificationTypes.info);
+    this.showNotification(message, notificationTypes.info);
   },
   error(message) {
-    this._showNotification(message, notificationTypes.error);
+    this.showNotification(message, notificationTypes.error);
   },
   warning(message) {
-    this._showNotification(message, notificationTypes.warning);
+    this.showNotification(message, notificationTypes.warning);
   },
 });
 
 const notify = Notification();
 
 export default notify;
-/* eslint-enable */
