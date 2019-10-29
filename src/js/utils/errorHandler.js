8100c47909ab10e2bf6abe445c5aa76cd2296feb
teomrd <mironidis.gr@gmail.com>
2019-10-29 20:36:15 +0000

    #commander fixes link on notes

--- src/js/utils/errorHandler.js
+++ src/js/utils/errorHandler.js
@@ -1,7 +1,7 @@
 import notify from "../notify";
 
 const errorHandler = error => {
-  const { message = "Unexpected error occured!" } = error;
+  const { message = "Unexpected error occurred!" } = error;
   console.log('error', error); // eslint-disable-line
   notify.error(message);
 };
