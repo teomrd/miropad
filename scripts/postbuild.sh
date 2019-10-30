6f1cbe9d627b8958b3bf891107fbde38cd9f8a39
teomrd <mironidis.gr@gmail.com>
2019-10-30 00:06:41 +0000

    deployment in a dump way still

--- scripts/postbuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-############################
-# postbuild.sh
-############################
-
-PACKAGE_VERSION=$(jq -r ".name" package.json)
-git add ./dist/* && git commit -m "Deployment $PACKAGE_VERSION" -n
