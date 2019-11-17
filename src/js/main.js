7a1613d454806e7ee194037d7d8ed6b348a98099
teomrd <mironidis.gr@gmail.com>
2019-11-17 11:10:50 +0000

    drop support for predictive typing it was an experimental feature not working pretty well and impacts significantly the performance

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,10 @@ import IPFS from "ipfs";
 import "../css/styles.css";
 import "../css/print.css";
 import "github-markdown-css";
-import storage from "./utils/localstorage";
 import welcomeUser from "./welcome";
 import errorHandler from "./utils/errorHandler";
 import search from "./utils/search";
 import { markDownIt } from "./toggleMarkDownViewer";
-import getCaretCoordinates from "textarea-caret";
 import select from "./utils/dom";
 import ipfs from "./utils/ipfs";
 import commander from "./components/commander/commander";
@@ -18,8 +16,6 @@ import {
 } from "./components/noteManager/noteManager";
 
 const main = async () => {
-  const suggestion = document.querySelector(".suggestion");
-  const terminal = document.querySelector(".terminal");
   window.addEventListener("error", errorHandler);
 
   welcomeUser();
@@ -38,52 +34,9 @@ const main = async () => {
           select(".logo").removeClass("unsaved");
         }
       }
-
       // MarkDown on the fly
       markDownIt();
-      const dictionary = storage.getDictionary();
-      const lastWord = select(".terminal")
-        .getValue()
-        .split(" ")
-        .pop();
-
-      if (lastWord.length > 1) {
-        const matches = dictionary.filter(word => word.startsWith(lastWord));
-        const firstMatch = matches.shift();
-        const prediction = firstMatch || "";
-        storage.set("prediction", prediction);
-        suggestion.innerHTML = prediction.replace(lastWord, "");
-      }
     });
-
-  terminal.addEventListener("input", function() {
-    var caret = getCaretCoordinates(this, this.selectionEnd);
-    suggestion.style.top = caret.top - caret.height / 3;
-    suggestion.style.left = caret.left;
-  });
-
-  terminal.addEventListener("keydown", async e => {
-    if (e.which === 9) {
-      e.preventDefault();
-      const pred = await localStorage.getItem("prediction");
-      const allTextArray = select(".terminal")
-        .getValue()
-        .split(" ");
-      select(".terminal")
-        .getValue()
-        .split(" ")
-        .pop();
-      allTextArray[allTextArray.length - 1] = pred;
-      allTextArray[allTextArray.length] = "";
-      if (pred && pred.length) {
-        select(".terminal").setValue(
-          allTextArray.toString().replace(/,/g, " ")
-        );
-        suggestion.innerHTML = "";
-        storage.set("prediction", "");
-      }
-    }
-  });
 
   const hash = window.location.hash.substr(1);
 
