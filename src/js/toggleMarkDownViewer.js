fadf1b903e30007333d9b53374aba98af49ac150
teomrd <mironidis.gr@gmail.com>
2019-11-24 23:01:05 +0000

    to support some nice github features

--- src/js/toggleMarkDownViewer.js
+++ src/js/toggleMarkDownViewer.js
@@ -1,12 +1,18 @@
 import showdown from "showdown";
 import select from "./utils/dom";
 
-const converter = new showdown.Converter();
+const converter = new showdown.Converter({
+  tasklists: true,
+  emoji: true,
+  simplifiedAutoLink: true,
+  tables: true,
+  openLinksInNewWindow: true
+});
+
+converter.setFlavor("github");
 
 export const markDownIt = () =>
-  select(".preview").html(
-    converter.makeHtml(document.querySelector(".terminal").value)
-  );
+  select(".preview").html(converter.makeHtml(select(".terminal").getValue()));
 
 const toggleMarkDownViewer = () => markDownIt().toggle();
 
