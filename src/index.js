2e7849c01d5dbe83cd4588187f3a3d6c055d3ce0
teomrd <mironidis.gr@gmail.com>
2019-12-11 22:08:04 +0000

    Deployment test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import main from "./js/main";
 import { registerServiceWorker } from "./js/registerServiceWorker";
 
 (() => {
-  console.log("VERSION ==> ", VERSION);
+  console.log("VERSION: ", VERSION);
   main();
   registerServiceWorker();
 })();
