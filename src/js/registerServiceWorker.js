be46d7c0a80e2cfdd6d117126b87248a40ef8a3a
teomrd <mironidis.gr@gmail.com>
2020-05-13 21:22:57 +0100

    Request notification permissions on a user action

--- src/js/registerServiceWorker.js
+++ src/js/registerServiceWorker.js
@@ -1,19 +1,16 @@
-const requestNotificationPermission = async () => {
+export const requestNotificationPermission = async () => {
   const permission = await window.Notification.requestPermission();
   if (permission !== "granted") {
     throw new Error("Permission not granted for Notification");
   }
+  // navigator.serviceWorker.controller.postMessage({ hello: "world" });
 };
 
-export const registerServiceWorker = () => {
+export const registerServiceWorker = async () => {
   if ("serviceWorker" in navigator) {
-    window.addEventListener("load", async () => {
-      navigator.serviceWorker.register("/sw.js");
-      await requestNotificationPermission();
-      await navigator.serviceWorker.register(
-        `${window.location.pathname}/serviceWorker.js?v=${VERSION}`
-      );
-      // navigator.serviceWorker.controller.postMessage({ hello: "world" });
-    });
+    navigator.serviceWorker.register("/sw.js");
+    await navigator.serviceWorker.register(
+      `${window.location.pathname}/serviceWorker.js?v=${VERSION}`
+    );
   }
 };
