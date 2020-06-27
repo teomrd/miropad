141dc97545443527fdef50d547f1ab48adfc4d6d
teomrd <mironidis.gr@gmail.com>
2020-06-27 14:48:10 +0100

    hashBrowser has one less dep

--- src/js/utils/hashBrowser.js
+++ src/js/utils/hashBrowser.js
@@ -1,9 +1,9 @@
-import IPFS from "ipfs";
+/* globals Buffer */
 import CID from "cids";
 import multihashing from "multihashing-async";
 
 const hashBrowser = async (val) => {
-  const hash = await multihashing(IPFS.Buffer.from(val), "sha2-256");
+  const hash = await multihashing(Buffer.from(val), "sha2-256");
   const cid = new CID(1, "dag-pb", hash);
   return cid.toString();
 };
