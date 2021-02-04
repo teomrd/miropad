38c9a346ecec9fafde192c91c83d722758a53bd9
teomrd <mironidis.gr@gmail.com>
2021-02-04 10:53:04 +0000

    ✨ feat: support html in md again

--- src/js/components/organisms/markdown/markDownViewer.js
+++ src/js/components/organisms/markdown/markDownViewer.js
@@ -2,7 +2,7 @@ import select from "../../../utils/dom";
 import { url } from "../../../utils/urlManager";
 import { copyToClipboard } from "../../../utils/copyToClipboard";
 import { button } from "../../atoms/button/button";
-import { convertMarkDownToHtml } from "./convertMarkDownToHtml";
+import { convertMarkDownToHtml } from "./mdToHtml";
 
 const markDownViewer = (() => {
   return {
@@ -20,31 +20,30 @@ const markDownViewer = (() => {
     },
     update: async function () {
       const md = select(".terminal").getValue();
-      const html = await convertMarkDownToHtml(md);
+      // const html = await convertMarkDownToHtml(md);
+      // this.view.innerHTML(html);
 
-      this.view.innerHTML(html);
+      this.view.innerHTML(convertMarkDownToHtml(md));
 
-      // this.view.innerHTML(converter.makeHtml(md));
+      const { elements } = select("pre");
+      Array.prototype.slice.call(elements).forEach((el) => {
+        const copyBtn = button("📋 Copy", async (e) => {
+          e.stopPropagation();
+          const codeToCopy = e.srcElement.previousSibling.innerHTML;
+          await copyToClipboard(codeToCopy, "📋 Code copied to clipboard");
+        });
+        el.appendChild(copyBtn);
+      });
 
-      // const { elements } = select("pre");
-      // Array.prototype.slice.call(elements).forEach((el) => {
-      //   const copyBtn = button("📋 Copy", async (e) => {
-      //     e.stopPropagation();
-      //     const codeToCopy = e.srcElement.previousSibling.innerHTML;
-      //     await copyToClipboard(codeToCopy, "📋 Code copied to clipboard");
-      //   });
-      //   el.appendChild(copyBtn);
-      // });
-
-      // select("code").listenAll("click", async ({ innerHTML }) => {
-      //   const result = eval(innerHTML);
-      //   select(".console").show().innerHTML(result);
-      // });
-      // select(".console").listen("click", async (e) => {
-      //   e.srcElement.classList.add("hidden");
-      //   const codeToCopy = e.srcElement.innerHTML;
-      //   await copyToClipboard(codeToCopy, "📋 Code copied to clipboard");
-      // });
+      select("code").listenAll("click", async ({ innerHTML }) => {
+        const result = eval(innerHTML);
+        select(".console").show().innerHTML(result);
+      });
+      select(".console").listen("click", async (e) => {
+        e.srcElement.classList.add("hidden");
+        const codeToCopy = e.srcElement.innerHTML;
+        await copyToClipboard(codeToCopy, "📋 Code copied to clipboard");
+      });
     },
     show: function (mode = true) {
       this.view.show();
---
+++ src/js/components/organisms/markdown/mdToHtml.js
@@ -0,0 +1,13 @@
+import showdown from "showdown";
+
+const converter = new showdown.Converter({
+  tasklists: true,
+  emoji: true,
+  simplifiedAutoLink: true,
+  tables: true,
+  openLinksInNewWindow: true,
+});
+
+converter.setFlavor("github");
+
+export const convertMarkDownToHtml = (md) => converter.makeHtml(md);
