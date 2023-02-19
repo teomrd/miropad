41628c8a1eb23685b9d9ce4b8eeb464f0af2f128
Theo Mironidis <mironidis.gr@gmail.com>
2023-02-19 23:19:11 +0000

    remove deprecated packages

---
+++ src/js/utils/hashBrowser.ts
@@ -0,0 +1,14 @@
+import { CID } from "multiformats/cid";
+import * as json from "multiformats/codecs/json";
+import { sha256 } from "multiformats/hashes/sha2";
+
+const hashBrowser = async (val) => {
+  const bytes = json.encode(val);
+
+  const hash = await sha256.digest(bytes);
+  const cid = CID.create(1, json.code, hash);
+
+  return cid.toString();
+};
+
+export default hashBrowser;
