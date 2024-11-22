562fc7faa11584f116060d64c2c6f5c23e95f717
teomrd <mironidis.gr@gmail.com>
2024-11-22 10:38:35 +0000

    feat(djot): log warnings

--- src/js/components/organisms/markdown/mdToHtml.ts
+++ src/js/components/organisms/markdown/mdToHtml.ts
@@ -1,5 +1,8 @@
 import { parse, renderHTML } from "@djot/djot";
 
 export const convertMarkDownToHtml = (md: string) => {
-  return renderHTML(parse(md, { sourcePositions: true }));
+  return renderHTML(parse(md, {
+    sourcePositions: true,
+    warn: (warning) => console.log(warning.render()),
+  }));
 };
