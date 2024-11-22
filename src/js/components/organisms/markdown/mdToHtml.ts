b0acd31aa2fa0f7d251a3df4a35bba9c1a35bd7c
teomrd <mironidis.gr@gmail.com>
2024-11-22 11:14:15 +0000

    feat(djot): remove positions for speed

--- src/js/components/organisms/markdown/mdToHtml.ts
+++ src/js/components/organisms/markdown/mdToHtml.ts
@@ -3,7 +3,6 @@ import { parse, renderHTML } from "@djot/djot";
 export const convertMarkDownToHtml = (md: string) => {
   return renderHTML(
     parse(md, {
-      sourcePositions: true,
       warn: (warning) => console.log(warning.render()),
     }),
   );
